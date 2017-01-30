a7a7f30cc2b010d36953c5d959e05b1ef142e306
Alex Baratti <alex.techtalk4geeks@gmail.com>
2017-01-29 17:09:26 -0800

    Added RGB Fragment

--- app/src/main/java/com/techtalk4geeks/ibis/RGBFragment.java
+++ app/src/main/java/com/techtalk4geeks/ibis/RGBFragment.java
@@ -1,9 +1,14 @@
 package com.techtalk4geeks.ibis;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 public class RGBFragment extends android.app.Fragment {
     @Override
@@ -14,5 +19,87 @@ public class RGBFragment extends android.app.Fragment {
 
         return inflater.inflate(
                 R.layout.fragment_rgb, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        final EditText rText = (EditText) getView().findViewById(R.id.rText);
+        rText.setFilters(new InputFilter[]{new InputFilterMinMax("0", "255")});
+        final EditText gText = (EditText) getView().findViewById(R.id.gText);
+        gText.setFilters(new InputFilter[]{new InputFilterMinMax("0", "255")});
+        final EditText bText = (EditText) getView().findViewById(R.id.bText);
+        bText.setFilters(new InputFilter[]{new InputFilterMinMax("0", "255")});
+
+        rText.addTextChangedListener(new TextWatcher() {
+
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
+                Integer textlength1 = rText.getText().length();
+
+                if (textlength1 >= 3) {
+                    gText.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+            }
+        });
+
+        gText.addTextChangedListener(new TextWatcher() {
+
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
+                Integer textlength2 = gText.getText().length();
+
+                if (textlength2 >= 3) {
+                    bText.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+            }
+        });
+
+        rText.setOnKeyListener(new View.OnKeyListener() {
+
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    // Perform action on Enter key press
+                    gText.requestFocus();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        gText.setOnKeyListener(new View.OnKeyListener() {
+
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    // Perform action on Enter key press
+                    bText.requestFocus();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
