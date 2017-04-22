f3b4b092da078cbff0496f0013c640a90273eada
Alex Baratti <alex.techtalk4geeks@gmail.com>
2017-04-22 15:26:07 -0700

    Created TicketView and made it a ratio of 4:1

---
+++ app/src/main/java/com/techtalk4geeks/ibis/TicketView.java
@@ -0,0 +1,106 @@
+package com.techtalk4geeks.ibis;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by alex on 4/22/17.
+ */
+
+public class TicketView extends View {
+    private Paint mTextPaint;
+    private String mText;
+    private int mAscent;
+
+    public TicketView(Context context) {
+        super(context);
+        initTicketView();
+    }
+
+    public TicketView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initTicketView();
+
+        TypedArray a = context.obtainStyledAttributes(attrs,
+                R.styleable.TicketView);
+
+        CharSequence s = a.getString(R.styleable.TicketView_android_text);
+        if (s != null) {
+            setText(s.toString());
+        }
+
+        setTextColor(a.getColor(R.styleable.TicketView_android_textColor, 0xFF000000));
+
+        int textSize = a.getDimensionPixelOffset(R.styleable.TicketView_android_textSize, 0);
+        if (textSize > 0) {
+            setTextSize(textSize);
+        }
+
+        a.recycle();
+    }
+
+    private void initTicketView() {
+        mTextPaint = new Paint();
+        mTextPaint.setAntiAlias(true);
+        // Must manually scale the desired text size to match screen density
+        mTextPaint.setTextSize(16 * getResources().getDisplayMetrics().density);
+        mTextPaint.setColor(0xFF000000);
+        setPadding(3, 3, 3, 3);
+    }
+
+    public void setText(String text) {
+        mText = text;
+        requestLayout();
+        invalidate();
+    }
+
+    public void setTextSize(int size) {
+        mTextPaint.setTextSize(size);
+        requestLayout();
+        invalidate();
+    }
+
+    public void setTextColor(int color) {
+        mTextPaint.setColor(color);
+        invalidate();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        setMeasuredDimension(measureWidth(widthMeasureSpec),
+                measureHeight(widthMeasureSpec));
+    }
+
+    private int measureWidth(int measureSpec) {
+        int result = 0;
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+
+        if (specMode == MeasureSpec.EXACTLY) {
+            result = specSize;
+        } else {
+            result = (int) mTextPaint.measureText(mText) + getPaddingLeft()
+                    + getPaddingRight();
+            if (specMode == MeasureSpec.AT_MOST) {
+                result = Math.min(result, specSize);
+            }
+        }
+
+        return result;
+    }
+
+    private int measureHeight(int measureSpec) {
+        int parentWidth = MeasureSpec.getSize(measureSpec);
+        return parentWidth / 4;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawText(mText, getPaddingLeft(), getPaddingTop() - mAscent, mTextPaint);
+    }
+}
